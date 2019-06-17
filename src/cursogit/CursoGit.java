10366db5b1165ad86747207e7bfb30c8fed2dc7a
ViniciusNogueiraS <v.nog@outlook.com>
2019-06-17 15:52:42 -0300

    pulo de uma linha e adesao de mais uma mensagem

--- src/cursogit/CursoGit.java
+++ src/cursogit/CursoGit.java
@@ -5,6 +5,8 @@ public class CursoGit {
     public static void main(String[] args) {
         System.out.print("olá!");
         System.out.print("tchau!");
+        System.out.print("\n");
+        System.out.print("olá denovo!");
     }
     
 }
